2c6f7ab4e4c93e4e958198a0150e8da8301ac82f
Luke <npv.loc@gmail.com>
2025-10-21 15:23:13 +0700

    Update

--- server.js
+++ server.js
@@ -8,10 +8,36 @@ app.listen(3000, function () {
 
 app.use("/register", registerRouter);
 
+app.use(express.json());
+app.use(express.urlencoded({ 
+  extended: true 
+}));
+
 app.get('/info', function(req, res) {
   res.send(JSON.stringify({name: "GDU", addr: "Quang Trung, Gò Vấp"}));
 });
 
+
+
+
 app.get('/', function(req, res) {
   res.send('Hello from ExpressJS');
+});
+
+app.post('/', function(req, res) {
+  const body = req.body;
+  console.log(JSON.parse(JSON.stringify(body)));
+  res.send('POST request to the homepage');
+});
+
+app.put('/', function(req, res) {
+  res.send('PUT request to the homepage');
+});
+
+app.patch('/', function(req, res) {
+  res.send('PATCH request to the homepage');
+});
+
+app.delete('/', function(req, res) {
+  res.send('DELETE request to the homepage');
 });
